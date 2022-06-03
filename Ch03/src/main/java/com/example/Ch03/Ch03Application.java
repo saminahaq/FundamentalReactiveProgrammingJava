495f9e4ee35017436af312b09a3212fec466eed4
saminahaq <saminanaseem2019@gmail.com>
2022-06-03 16:16:38 -0700

    Ch03

---
+++ Ch03/src/main/java/com/example/Ch03/Ch03Application.java
@@ -0,0 +1,13 @@
+package com.example.Ch03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch03Application.class, args);
+	}
+
+}
